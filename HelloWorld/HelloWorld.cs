00f43176188a85315968de3cfe09f4434b66f5ef
Andy Ko <peanut61313@gmail.com>
2018-10-01 19:43:27 -0500

    Updating the personalized message

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Andy!");
+            Console.WriteLine("Hello Andy Ko!");
         }
     }
 }
