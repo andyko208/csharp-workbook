4dc9116f55595842ecfbe953bdcddeadb6bbf139
Andy Ko <peanut61313@gmail.com>
2018-10-08 21:01:22 -0500

    just need to get checked

--- PigLatin/PigLatin.cs
+++ PigLatin/PigLatin.cs
@@ -6,15 +6,39 @@ namespace PigLatin
     {
         public static void Main()
         {
-            // your code goes here
-
+            Console.WriteLine("Enter a word: ");
             // leave this command at the end so your program does not close automatically
-            Console.ReadLine();
+            String inWord = Console.ReadLine();
+            Console.WriteLine("Your word became " + TranslateWord(inWord));
         }
         
         public static string TranslateWord(string word)
         {
-            // your code goes here
+            int firstLetterIndex = word.IndexOfAny(new char[] {'a', 'i', 'u', 'e', 'o'});
+            int yChecker = word.IndexOfAny(new char[] {'y'});   //separate var checks if 'y' works as a vowel
+            String part = "";   //the part that gets cut out
+            if(firstLetterIndex == 0)   //#1 condition when first letter = vowel
+            {
+                part = word.Substring(0, firstLetterIndex);
+                word = word.Substring(firstLetterIndex);
+                word += part + "yay";
+            }
+            else if(yChecker >= 1 && firstLetterIndex > yChecker) //#2 condition when 'y' is used as a vowel
+            {                                                     //only works when 'y' is before any other vowels
+                part = word.Substring(0, yChecker);
+                word = word.Substring(yChecker);
+                word += part + "ay";
+            }
+            else if(firstLetterIndex >= 1)                //#3 condition when vowels, not 'y', exists but not 'y'
+            {
+                part = word.Substring(0, firstLetterIndex);
+                word = word.Substring(firstLetterIndex);
+                word += part + "ay";
+            }
+            else            //#4 condition when there are no vowels
+            {
+                word += "yay";
+            }
             return word;
         }
     }
