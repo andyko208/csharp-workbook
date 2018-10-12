80d5eb2efaac55df00c60c7f6254cd3076668f15
Andy Ko <peanut61313@gmail.com>
2018-10-12 11:26:16 -0500

    ToLower() added and change var name

--- PigLatin/PigLatin.cs
+++ PigLatin/PigLatin.cs
@@ -9,6 +9,7 @@ namespace PigLatin
             // Console.WriteLine("Enter a word: ");
             // // leave this command at the end so your program does not close automatically
             // String inWord = Console.ReadLine();
+            // inWord = inWord.ToLower();
             // Console.WriteLine("Your word became " + TranslateWord(inWord));
             if(tests()){
                 Console.WriteLine("Tests passed.");
@@ -28,25 +29,25 @@ namespace PigLatin
         }
         public static string TranslateWord(string word)
         {
-            int firstLetterIndex = word.IndexOfAny(new char[] {'a', 'i', 'u', 'e', 'o'});
+            int firstVowelIndex = word.IndexOfAny(new char[] {'a', 'i', 'u', 'e', 'o'});
             int yChecker = word.IndexOfAny(new char[] {'y'});   //separate var checks if 'y' works as a vowel
             String part = "";   //the part that gets cut out
-            if(firstLetterIndex == 0)   //#1 condition when first letter = vowel
+            if(firstVowelIndex == 0)   //#1 condition when first letter = vowel
             {
-                part = word.Substring(0, firstLetterIndex);
-                word = word.Substring(firstLetterIndex);
+                part = word.Substring(0, firstVowelIndex);
+                word = word.Substring(firstVowelIndex);
                 word += part + "yay";
             }
-            else if(yChecker >= 1 && firstLetterIndex > yChecker) //#2 condition when 'y' is used as a vowel
+            else if(yChecker >= 1 && firstVowelIndex > yChecker) //#2 condition when 'y' is used as a vowel
             {                                                     //only works when 'y' is before any other vowels
                 part = word.Substring(0, yChecker);
                 word = word.Substring(yChecker);
                 word += part + "ay";
             }
-            else if(firstLetterIndex >= 1)                //#3 condition when vowels, not 'y', exists but not 'y'
+            else if(firstVowelIndex >= 1)                //#3 condition when vowels, not 'y', exists but not 'y'
             {
-                part = word.Substring(0, firstLetterIndex);
-                word = word.Substring(firstLetterIndex);
+                part = word.Substring(0, firstVowelIndex);
+                word = word.Substring(firstVowelIndex);
                 word += part + "ay";
             }
             else            //#4 condition when there are no vowels
