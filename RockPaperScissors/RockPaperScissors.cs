7a502e888ca30171103c69dba17ae7b55b3a23fd
Andy Ko <peanut61313@gmail.com>
2018-10-11 19:11:15 -0500

    RockPaperScissors

--- RockPaperScissors/RockPaperScissors.cs
+++ RockPaperScissors/RockPaperScissors.cs
@@ -19,6 +19,43 @@ namespace RockPaperScissors
         public static string CompareHands(string hand1, string hand2)
         {
             // Your code here
+            if(hand1.Equals(hand2))
+            {
+                return "It's a tie!";
+            }
+            if(hand1.Equals("rock"))
+            {
+                if (hand2.Equals("scissors"))
+                {
+                    return "Hand one wins!";
+                }
+                else            //if hand2 is paper
+                {
+                    return "Hand two wins!";
+                }
+            }
+            if(hand1.Equals("scissors"))
+            {
+                if (hand2.Equals("paper"))
+                {
+                    return "Hand one wins!";
+                }
+                else            //if hand2 is rock
+                {
+                    return "Hand two wins!";
+                }
+            }
+            if(hand1.Equals("paper"))
+            {
+                if (hand2.Equals("rock"))
+                {
+                    return "Hand one wins!";
+                }
+                else            //if hand2 is scissors
+                {
+                    return "Hand two wins!";
+                }
+            }
             return hand1 + ' ' + hand2;
         }
     }
