e5ebadd93f781e9d34eb326e7b90986f2a75ef30
Andy Ko <peanut61313@gmail.com>
2018-10-15 23:48:41 -0500

    working on PlaceMark

--- TicTacToe/TicTacToe.cs
+++ TicTacToe/TicTacToe.cs
@@ -18,25 +18,41 @@ namespace TicTacToe
             {
                 DrawBoard();
                 GetInput();
+                if(PlaceMark() == true)
+                {
+                    board[row][column] = playerTurn;
+                }
 
             } while (!CheckForWin() && !CheckForTie());
 
             // leave this command at the end so your program does not close automatically
             Console.ReadLine();
         }
-
-        public static void GetInput()
+        
+        public static int[] GetInput()           //return two ints that store user inputs
         {
             Console.WriteLine("Player " + playerTurn);
             Console.WriteLine("Enter Row:");
             int row = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter Column:");
             int column = int.Parse(Console.ReadLine());
+            int[] taken = new[] {row, column};
+            return taken;
+            if(PlaceMark(row, column) == true)
+            {
+                playerTurn = "O";
+            } 
         }
-
-        public static void PlaceMark(int row, int column)
+        public static bool PlaceMark(int row, int column) //return true if possible to put a mark
         {
-        // your code goes here
+            // your code goes here
+            if(row > 2 || column > 2)
+                return false;
+            if(board[row][column] == "X" || board[row][column] == "O")
+                return false;
+            board[row][column] = playerTurn;
+            return true;
+            
         }
 
         public static bool CheckForWin()
