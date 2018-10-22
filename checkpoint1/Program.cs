b82cd8a471ffcbca227dea521b7eccd7ee318b21
Andy Ko <peanut61313@gmail.com>
2018-10-22 18:34:52 -0500

    checkpoint1 initial push

---
+++ checkpoint1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace checkpoint1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public static int count()
+        {
+            return 0;
+        }
+    }
+}
