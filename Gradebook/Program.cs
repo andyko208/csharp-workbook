80508646b19612b7f245f681443069f8177480fc
Andy Ko <peanut61313@gmail.com>
2018-10-24 20:06:04 -0500

    dictionary added

---
+++ Gradebook/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Gradebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<String, int[]> grades = new Dictionary<string, int[]>();     
+            Console.WriteLine("What's the name of the student?");
+            
+        }
+    }
+}
