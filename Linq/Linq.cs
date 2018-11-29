a4c1799b35a6dfdda56f60f1a2d82d7bb77c4be5
Andy Ko <peanut61313@gmail.com>
2018-11-28 19:52:54 -0600

    negative student balance

--- Linq/Linq.cs
+++ Linq/Linq.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿using System.Linq;
+using System.Collections.Generic;
+using System;
 
-namespace Linq
+namespace StudentList
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	public class Program
+	{
+		public static void Main()
+		{
+            List<Student> students = new List<Student>();
+            
+            students.Add(new Student("Chris", "123-456-7891", "123 Delany", -2990));
+            students.Add(new Student("Kevin", "512-222-2222", "435 Carolyn", -2500));
+            students.Add(new Student("Victoria", "512-827-8498", "701 Brazos St", 0));
+            students.Add(new Student("Luke", "555-555-5555", "451 Brody Ln", -500));
+            
+            //your code here
+            IEnumerable<Student> negValue = from stu in students
+                where stu.Balance < 0
+                select stu;
+            foreach(Student st in negValue)
+            {
+                Console.WriteLine("{0} has a negative tuition balance.",st.Name);
+            }
+		}
+
+	}
+	
+	public class Student
+	{
+		public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+		public int Balance { get; set; }
+		
+		public Student (string name, string phone, string address, int balance)
+		{
+			Name = name;
+			Phone = phone;
+			Address = address;
+			Balance = balance;
+		}
+	}
 }
