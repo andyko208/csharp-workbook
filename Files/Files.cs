554985ba75e5d63ed2ff90c4b0bcc8841e09ca26
Andy Ko <peanut61313@gmail.com>
2018-12-04 00:35:57 -0600

    need to set the random num and generate random word

--- Files/Files.cs
+++ Files/Files.cs
@@ -1,12 +1,20 @@
 ﻿using System;
-
+using System.IO;
+using System.Collections.Generic;
 namespace Files
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String file = @"/Users/andyko/Downloads/words_alpha.txt";
+            String copy = @"/Users/andyko/Desktop/copy.txt";
+            allStrings(file, copy);
+            string[] wordsFile = File.ReadAllLines(file);
+        }
+        public static void allStrings(String source, String dest){
+            String[] words = File.ReadAllLines(source);
+            File.WriteAllLines(dest, words);
         }
     }
 }
