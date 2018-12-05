d639138d96a05551f1a887b3ed14314cb4829390
Andy Ko <peanut61313@gmail.com>
2018-12-05 15:54:54 -0600

    final code for Files

--- Files/Files.cs
+++ Files/Files.cs
@@ -8,13 +8,28 @@ namespace Files
         static void Main(string[] args)
         {
             String file = @"/Users/andyko/Downloads/words_alpha.txt";
-            String copy = @"/Users/andyko/Desktop/copy.txt";
-            allStrings(file, copy);
+            // String copy = @"/Users/andyko/Desktop/copy.txt";
+            // allStrings(file, copy);
             string[] wordsFile = File.ReadAllLines(file);
+            Random rnd = new Random();
+            int randIndex = rnd.Next(0, wordsFile.Length);
+            Console.WriteLine(wordsFile[randIndex]);
+            String ans = "";                            //user input string
+            String randAns = wordsFile[randIndex];      //generate a random word from the array
+            while(ans != randAns)
+            {
+                Console.WriteLine("Guess a word:");
+                ans = Console.ReadLine();
+                if(Array.IndexOf(wordsFile, ans) < Array.IndexOf(wordsFile, randAns))
+                    Console.WriteLine("Your answer is before the actual one.");
+                if(Array.IndexOf(wordsFile, ans) > Array.IndexOf(wordsFile, randAns))
+                    Console.WriteLine("Your answer is after the actual one.");
+            }
+            Console.WriteLine("You got it!");
         }
-        public static void allStrings(String source, String dest){
-            String[] words = File.ReadAllLines(source);
-            File.WriteAllLines(dest, words);
-        }
+        // public static void allStrings(String source, String dest){
+        //     String[] words = File.ReadAllLines(source);
+        //     File.WriteAllLines(dest, words);
+        // }
     }
 }
